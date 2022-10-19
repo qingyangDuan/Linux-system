49b6d7931c47d5bfb630a9d95896067764365017
Qingyang Duan <duanqy99@163.com>
2022-10-19 12:25:51 +0800

    Update NIC-driver-problem.sh

--- NIC-driver-problem.sh
+++ NIC-driver-problem.sh
@@ -2,5 +2,7 @@
 lspci |grep Eth
 lshw -C network 
 
-download driver from https://www.mellanox.com/products/ethernet-drivers/linux/mlnx_en
+# install MLNX OFED driver  
+download driver from https://network.nvidia.com/products/infiniband-drivers/linux/mlnx_ofed/  
+安装 MLNX_OFED 4.9-x LTS 版即可，这一版本可支持connectX3 - connectX6的很多网卡。最新版本OFED容易出错，不建议安装。
 tar and install
